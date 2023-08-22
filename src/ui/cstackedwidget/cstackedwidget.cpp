4199ec936483e5dd6804352797fe9bb1f28f706d
mistika <mistikasoft@gmail.com>
2023-08-22 21:37:16 +0200

    add CStackedWidget class

---
+++ src/ui/cstackedwidget/cstackedwidget.cpp
@@ -0,0 +1,6 @@
+#include "cstackedwidget.h"
+
+CStackedWidget::CStackedWidget()
+{
+
+}
---
+++ src/ui/cstackedwidget/cstackedwidget.h
@@ -0,0 +1,13 @@
+#ifndef CSTACKEDWIDGET_H
+#define CSTACKEDWIDGET_H
+
+#include <QStackedWidget>
+#include <QObject>
+
+class CStackedWidget : public QStackedWidget
+{
+public:
+    CStackedWidget();
+};
+
+#endif // CSTACKEDWIDGET_H
