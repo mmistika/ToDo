e2d60ba8fddd6e039af143ab1fb95f7c78a41e50
mistika <mistikasoft@gmail.com>
2023-08-23 17:17:15 +0200

    implement CStackedWidget for convenient page management in QStackedWidget

--- src/ui/cstackedwidget/cstackedwidget.cpp
+++ src/ui/cstackedwidget/cstackedwidget.cpp
@@ -1,6 +1,50 @@
 #include "cstackedwidget.h"
 
-CStackedWidget::CStackedWidget()
-{
+#include <QStackedLayout>
 
+CStackedWidget::CStackedWidget(StackingMode stackingMode)
+{
+    _layout = static_cast<QStackedLayout*>(layout());
+    setStackingMode(stackingMode);
+}
+
+void CStackedWidget::setStackingMode(StackingMode stackingMode)
+{
+    _layout->setStackingMode(static_cast<QStackedLayout::StackingMode>(stackingMode));
+}
+
+void CStackedWidget::setTopMost(QWidget *page)
+{
+    int index = indexOf(page);
+    if (index != -1 and page != _topMostPage)
+    {
+        _topMostPage = page;
+        _topMostPage->raise();
+    }
+}
+
+void CStackedWidget::setTopMost(int index)
+{
+    setTopMost(widget(index));
+}
+
+void CStackedWidget::removeTopMost()
+{
+    _topMostPage = nullptr;
+}
+
+void CStackedWidget::raisePage(QWidget *page)
+{
+    int index = indexOf(page);
+    if (index != -1 and index != currentIndex())
+    {
+        if (_topMostPage) page->stackUnder(_topMostPage);
+        else page->raise();
+    }
+}
+
+void CStackedWidget::raisePage(int index)
+{
+    QWidget *wgt = widget(index);
+    if (wgt) raisePage(wgt);
 }
--- src/ui/cstackedwidget/cstackedwidget.h
+++ src/ui/cstackedwidget/cstackedwidget.h
@@ -2,12 +2,35 @@
 #define CSTACKEDWIDGET_H
 
 #include <QStackedWidget>
-#include <QObject>
+
+class QStackedLayout;
 
 class CStackedWidget : public QStackedWidget
 {
+    Q_OBJECT
+
 public:
-    CStackedWidget();
+    enum StackingMode {
+        StackOne,
+        StackAll
+    };
+
+public:
+    CStackedWidget(StackingMode stackingMode);
+
+    void setStackingMode(StackingMode stackingMode);
+
+    void setTopMost(QWidget *page);
+    void setTopMost(int index);
+    void removeTopMost();
+
+    void raisePage(QWidget *page);
+    void raisePage(int index);
+
+private:
+    QStackedLayout *_layout{nullptr};
+
+    QWidget *_topMostPage{nullptr};
 };
 
 #endif // CSTACKEDWIDGET_H
