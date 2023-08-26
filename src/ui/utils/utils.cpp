d9605e99a50810d79be264fc8cb865aa95715573
mistika <mistikasoft@gmail.com>
2023-08-26 19:04:53 +0200

    add 'getRoundedRegion' to 'utils'

--- src/ui/utils/utils.cpp
+++ src/ui/utils/utils.cpp
@@ -21,4 +21,35 @@ void setShadow(const QColor &color, const QPoint &offset, const int &blurRadius,
     wgt->setGraphicsEffect(shadowEffect);
 }
 
+QRegion getRoundedRegion(QRect rect, int radius)
+{
+    if (!radius)
+    {
+        return QRegion(rect);
+    }
+    else if (radius == rect.width() / 2 and radius == rect.height() / 2)
+    {
+        return QRegion(rect, QRegion::Ellipse);
+    }
+    else
+    {
+        QRect hrRect = rect.marginsRemoved(QMargins(0, radius, 0, radius));
+        QRect vrRect = rect.marginsRemoved(QMargins(radius, 0, radius, 0));
+
+        int diameter = radius * 2;
+        QRegion cornerTopLeft = QRegion(hrRect.x(), vrRect.y(), diameter, diameter, QRegion::Ellipse);
+        QRegion cornerTopRight = cornerTopLeft.translated(vrRect.width(), 0);
+        QRegion cornerBottomLeft = cornerTopLeft.translated(0, hrRect.height());
+        QRegion cornerBottomRight = cornerTopRight.translated(0, hrRect.height());
+
+        return QRegion()
+               + hrRect
+               + vrRect
+               + cornerTopLeft
+               + cornerTopRight
+               + cornerBottomLeft
+               + cornerBottomRight;
+    }
+}
+
 }
--- src/ui/utils/utils.h
+++ src/ui/utils/utils.h
@@ -9,6 +9,8 @@ namespace ui_utils
 void setShadow(const QColor &color, const QPoint &offset, const int &blurRadius, QWidget *wgt);
 void setShadow(const QColor &color, const QPoint &offset, const int &blurRadius, QWidgetList &wgtList);
 
+QRegion getRoundedRegion(QRect rect, int radius = 0);
+
 }
 
 #endif // UTILS_H
