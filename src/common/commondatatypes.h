0612751a65207448f2eabe444e8d9ede9c64c52b
mistika <mistikasoft@gmail.com>
2023-09-08 20:32:46 +0200

    add common DTOs and data types

---
+++ src/common/commondatatypes.h
@@ -0,0 +1,44 @@
+#ifndef COMMONDATATYPES_H
+#define COMMONDATATYPES_H
+
+#include <vector>
+
+#include <QString>
+
+namespace Common
+{
+struct Task
+{
+    enum class State
+    {
+        Incompleted,
+        Completed
+    };
+
+    int id;
+    State state;
+    QString text;
+    bool isImportant;
+    QString dueDate;
+};
+
+using TaskList = std::vector<Task>;
+
+struct Group
+{
+    int id;
+    QString title;
+};
+
+using GroupList = std::vector<Group>;
+
+struct Tag
+{
+    int id;
+    QString title;
+};
+
+using TagList = std::vector<Tag>;
+}
+
+#endif // COMMONDATATYPES_H
